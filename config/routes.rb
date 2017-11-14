1ca46b640a0c13e22a1be5937d8ea83d7c42925c
Alex Olthoff <m.alex.olthoff@gmail.com>
2017-11-14 14:12:38 -0500

    added routes for today's page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+
+  resources :users, only: [:show, :edit, :update]
+  get 'dashboard', to: 'pages#dashboard'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20171113192306) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,19 +38,9 @@
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-<<<<<<< HEAD
     t.string   "provider"
     t.string   "uid"
     t.string   "facebook_picture_url"
-=======
-
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "facebook_picture_url"
-    t.string   "token"
-    t.datetime "token_expiry"
-
->>>>>>> 70567c5f696b8e39b1d00f80732d68263ae21c4c
     t.string   "first_name"
     t.string   "last_name"
     t.string   "token"
@@ -56,7 +48,6 @@
     t.date     "DOB"
     t.integer  "number_countries"
     t.integer  "years"
-
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
