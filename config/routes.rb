8d631823dcf3fd0606dfac1c24f00c3362dbec4b
Alex Olthoff <m.alex.olthoff@gmail.com>
2017-11-14 16:25:05 -0500

    deleted more unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
-  get 'countries/show'
-
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
-  resources :countries, only: [:show]
 
   root to: 'pages#home'
 
   resources :users, only: [:show, :edit, :update]
-  get 'destination_selector', to: 'pages#dashboard'
   resources :destinations, only: [:create]
   resources :countries, only: [:show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'destination_selector', to: 'pages#dashboard'
 end
