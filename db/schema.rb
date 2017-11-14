9286edd6457f7276014e60038bbe3d4b6e03d15a
Jane Costa <yberdysh@gmail.com>
2017-11-14 15:52:48 -0500

    seeded countries and added countries show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113212215) do
+ActiveRecord::Schema.define(version: 20171114203001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20171113212215) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "continent"
   end
 
   create_table "destinations", force: :cascade do |t|
