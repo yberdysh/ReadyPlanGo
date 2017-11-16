60f54b5d73d610abb27666be86c26a7aaa137684
Lucia Mkhitarova <lucia.mkhitarova@gmail.com>
2017-11-16 17:24:24 -0500

    fixed navbar issues, fixed tab logo, and tried to route user to dashboard:

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
+
+  authenticated :user do
+    root to: 'pages#destination_select'
+  end
+
   root to: 'pages#home'
 
   resources :users, only: [:show, :edit, :update]
