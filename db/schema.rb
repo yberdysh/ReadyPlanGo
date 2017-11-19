9147de7c70f127960112d83d2adc0ac12e2f204c
Alex Olthoff <m.alex.olthoff@gmail.com>
2017-11-19 15:23:16 -0500

    Finished cloudinairy and carrierwave. This allows for upload to the dashboard using the url, but is not perfect. There is still work to be done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119041258) do
+ActiveRecord::Schema.define(version: 20171119191628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20171119041258) do
     t.date     "DOB"
     t.integer  "number_countries"
     t.integer  "years"
+    t.string   "photo"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
