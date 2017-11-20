9aaafbcdfec357de58a3a7b209f4e7c2f39eeb93
Jane Costa <yberdysh@gmail.com>
2017-11-20 16:22:21 -0500

    updated goal now saving to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :photo, :photo_cache)
+    params.require(:user).permit(:first_name, :last_name, :photo, :photo_cache, :number_countries, :years)
   end
 
   def set_user
