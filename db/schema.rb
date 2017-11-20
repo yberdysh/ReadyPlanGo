e4b9e8af2ef66ec0724ba03b96b13cd32ce7f86b
Alex Olthoff <m.alex.olthoff@gmail.com>
2017-11-20 15:08:43 -0500

    Added config for AJAX

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171120180828) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 20171120180828) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.string   "continent"
+    t.string   "airport_code"
     t.float    "latitude"
     t.float    "longitude"
-    t.string   "airport_code"
     t.string   "currency_name"
     t.string   "capital"
     t.string   "population"
