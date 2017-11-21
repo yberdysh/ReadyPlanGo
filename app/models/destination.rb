4db55df35fb8b66a87dccaf9e3ad5723e89d65dc
Lucia Mkhitarova <lucia.mkhitarova@gmail.com>
2017-11-21 15:29:20 -0500

    styling dest_select page

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -3,6 +3,7 @@ class Destination < ApplicationRecord
   validates :country_id, uniqueness: true
   DESTINATION_TYPES = [
     {
+      # title: "<i class="fa fa-star-o" aria-hidden="true"></i><p>Countries I've Visited</p>".html_safe,
       title: "Countries I've Visited",
       status: 1
     },
