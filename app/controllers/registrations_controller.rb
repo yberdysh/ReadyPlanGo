374906799ee6327dfd72165afb7f1445b9a522f8
Alex Olthoff <m.alex.olthoff@gmail.com>
2017-11-22 18:11:53 -0500

    fixed controller to accept params for airport code

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :number_countries, :years, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :number_countries, :years, :email, :password, :password_confirmation, :airport_code)
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :number_countries, :years, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:first_name, :last_name, :number_countries, :years, :email, :password, :password_confirmation, :current_password, :airport_code)
   end
 end
--- db/migrate/20171122222239_add_airportcode_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddAirportcodeToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :airport_code, :string
-  end
-end
