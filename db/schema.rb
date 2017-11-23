b54df6d1e354378c75e3049ec4f69cae90f67dcf
Lucia Mkhitarova <lucia.mkhitarova@gmail.com>
2017-11-23 10:29:16 -0500

    working on legend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121210705) do
+ActiveRecord::Schema.define(version: 20171122202104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 20171121210705) do
     t.integer  "number_countries"
     t.integer  "years"
     t.string   "photo"
+    t.string   "airport_code"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
