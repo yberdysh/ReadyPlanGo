98e0d2da6d911397cb352581bacfa4ac4e0a8f67
Lucia Mkhitarova <lucia.mkhitarova@gmail.com>
2017-11-23 12:34:26 -0500

    fixed city saving issue

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -6,17 +6,20 @@ class Destination < ApplicationRecord
       # title: "<i class="fa fa-star-o" aria-hidden="true"></i><p>Countries I've Visited</p>".html_safe,
       title: "Countries I've Been To",
       status: 1,
-      id: "visited"
+      id: "visited",
+      icon: '<i class="fa fa-star-o" aria-hidden="true"></i>'
     },
     {
       title: "Countries I Want to Visit",
       status: 2,
-      id: "toVisit"
+      id: "toVisit",
+      icon: '<i class="fa fa-plus-circle" aria-hidden="true"></i>'
     },
     {
       title: "Top Priority",
       status: 3,
-      id: "visitSoon"
+      id: "visitSoon",
+      icon: '<i class="fa fa-check-circle-o" aria-hidden="true"></i>'
     }
   ]
 end
